7d7b85fd027e32052300077ee2254506bfb06465
Erik van der Bas - PC <erik@basedonline.nl>
2019-03-08 12:35:10 +0100

    init commit of BaseBuilder webpack config

---
+++ bin/basebuilder-config.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+'use strict';
+
+// Update notifier.
+const updateNotifier = require( 'update-notifier' );
+const pkg = require( '../package.json' );
+updateNotifier( { pkg } ).notify();
+
+//const spawn = require( 'cgb-dev-utils/crossSpawn' );
+const args = process.argv.slice( 2 );
+
+const scriptIndex = args.findIndex(
+	// x => x === 'build' || x === 'eject' || x === 'start' || x === 'test'
+	x => x === 'start' || x === 'build' || x === 'eject' || x === 'test'
+);
+const script = scriptIndex === -1 ? args[ 0 ] : args[ scriptIndex ];
+const nodeArgs = scriptIndex > 0 ? args.slice( 0, scriptIndex ) : [];
+
+switch ( script ) {
+	case 'production':
+	case 'development':
+	case 'watch':
+	default:
+		console.log( 'Unknown script "' + script + '".' );
+		console.log( 'Perhaps you need to update cgb-scripts?' );
+		console.log(
+			'Update via: npm install -g create-guten-block or npm install cgb-scripts'
+		);
+		break;
+}
