ce1b23f1894ec88f2a16c38f95e76865b350a49b
Erik van der Bas - PC <erik@basedonline.nl>
2019-03-09 12:02:28 +0100

    move postscss

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -65,7 +65,7 @@ const webpackConfig = {
                         loader: 'postcss-loader',
                         options: {
                             config: {
-                                path: __dirname + '/postcss.config.js',
+                                path: __dirname + '/config/postcss.config.js',
                             },
                         },
                     },
