3ca37cf1c17e23e5a9d8a97e36fd6c0395ad39c7
Erik van der Bas - Laptop <erik@basedonline.nl>
2019-03-12 17:46:10 +0100

    new modules and working on messages.

--- scripts/development.js
+++ scripts/development.js
@@ -4,5 +4,27 @@ process.env.NODE_ENV = 'development';
 const webpack = require("webpack");
 const webpackConfig = require( '../config/webpack.config' );
 const compiler = webpack( webpackConfig );
+const formatMessages = require('webpack-format-messages');
+const chalk = require( 'chalk' );
 
-const watching = compiler.run();
+compiler.run( ( err, stats ) => {
+  const messages = formatMessages(stats);
+  console.log( `\n${ chalk.dim( 'Let\'s build and compile the files...' ) }` );
+  console.log(messages);
+  if (!messages.errors.length && !messages.warnings.length) {
+    console.log( '\n✅ ', chalk.black.bgGreen( ' Compiled successfully! \n' ) );
+  }
+
+  if (messages.errors.length) {
+    console.log( '\n❌ ', chalk.black.bgRed( ' Failed to compile build. \n' ) );
+    console.log( '\n👉 ', messages.errors.join( '\n\n' ) );
+    return;
+  }
+
+  if (messages.warnings.length) {
+    console.log( '\n❌ ', chalk.yellow( ' Compiled with warnings. \n' ) );
+    console.log( '\n👉 ', messages.warnings.join( '\n\n' ) );
+
+  }
+
+});
