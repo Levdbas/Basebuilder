dc4fa9a3e426f0c50226caeddba21497c40c007f
Erik van der Bas - Laptop <erik@basedonline.nl>
2019-09-03 12:02:34 +0200

    setting publicpath via window object.

--- config.js
+++ config.js
@@ -2,22 +2,26 @@ const path = require('path');
 const rootPath = process.cwd();
 const merge = require('webpack-merge');
 const watchMode = global.watch || false;
-var userConfig = require(path.resolve(__dirname, rootPath) + '/assets/config.json');
+var userConfig = require(path.resolve(__dirname, rootPath) +
+	'/assets/config.json');
 
-var config = merge(
-    {
-        path: {
-            theme: path.join(rootPath, userConfig['themePath']), // from root folder path/to/theme
-            dist: path.join(rootPath, userConfig['themePath'] + '/dist/'), // from root folder path/to/theme
-            assets: path.join(rootPath, userConfig['assetsPath']), // from root folder path/to/assets
-            public: watchMode ? userConfig['publicPath'] + 'dist/' : '/',
-        },
-    },
-    userConfig,
+var config = merge({
+		path: {
+			theme: path.join(rootPath, userConfig[
+			'themePath']), // from root folder path/to/theme
+			dist: path.join(rootPath, userConfig['themePath'] +
+			'/dist/'), // from root folder path/to/theme
+			assets: path.join(rootPath, userConfig[
+			'assetsPath']), // from root folder path/to/assets
+			public: watchMode ? userConfig['publicPath'] + 'dist/' : '/',
+		},
+	},
+	userConfig,
 );
 
 if (watchMode) {
-    config.entry.app.push('webpack-hot-middleware/client');
+	config.entry.app.push('webpack-hot-middleware/client');
+	config.entry.app.push('basebuilder-config/publicpath_entry.js');
 }
 
 module.exports = config;
