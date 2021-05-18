09cb86ee211de789d06c25f256137b724813dd3c
Erik van der Bas <erik@basedonline.nl>
2021-05-18 10:43:52 +0200

    continue on webpack 5

--- src/build/webpack.config.js
+++ src/build/webpack.config.js
@@ -24,147 +24,147 @@ const config = require('../config');
 const CreateSourceMap = devMode ? config.sourceMaps : false;
 
 const webpackConfig = {
-	mode: env,
-	context: config.path.assets,
-	entry: config.entry,
-	devtool: CreateSourceMap ? 'inline-cheap-module-source-map' : false,
-	watch: watchMode,
-	output: {
-		filename: devMode ? 'scripts/[name].js' : 'scripts/[name].[contenthash].js',
-		chunkFilename: devMode ? 'scripts/[name].bundle.js' : 'scripts/[name].bundle.[contenthash].js',
-		path: config.path.dist,
-		publicPath: config.path.public,
-		pathinfo: false,
-	},
-	performance: { hints: false },
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules[\/\\](?!(swiper|dom7|sal.js)[\/\\])/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						cacheDirectory: true,
-						presets: [['@babel/preset-env', { targets: 'defaults' }]],
-					},
-				},
-			},
-			{
-				test: /\.vue$/,
-				loader: 'vue-loader',
-			},
-			{
-				test: /\.scss$/,
-				exclude: /node_modules/,
-				use: [
-					{
-						loader: MiniCssExtractPlugin.loader,
-						options: {
-							publicPath: '../',
-						},
-					},
-					{
-						loader: 'css-loader',
-					},
-					{
-						loader: 'postcss-loader',
-						options: {
-							postcssOptions: {
-								config: path.resolve(__dirname, 'postcss.config.js'),
-							},
-						},
-					},
-					{
-						loader: 'sass-loader',
-					},
-				],
-			},
-			{
-				test: /\.(ttf|eot|woff2?|png|jpe?g|gif|svg|ico)$/,
-				include: config.path.urlLoaderAssets,
-				loader: 'url-loader',
-				options: {
-					limit: 4096,
-					name: devMode ? '[path][name].[ext]' : '[path][name].[contenthash].[ext]',
-				},
-			},
-		],
-	},
-	resolve: {
-		alias: {
-			acfBlocks: path.resolve(__dirname, config.path.theme + '/partials/blocks'),
-			twigBlocks: path.resolve(__dirname, config.path.theme + '/resources/views/blocks'),
-			parentThemeStyles: path.resolve(__dirname, config.path.parentThemeAssets + 'styles'),
-			parentThemeScripts: path.resolve(__dirname, config.path.parentThemeAssets + 'scripts'),
-			parentThemeFonts: path.resolve(__dirname, config.path.parentThemeAssets + 'fonts'),
-			parentThemeImages: path.resolve(__dirname, config.path.parentThemeAssets + 'images'),
-		},
-	},
-	externals: config.externals,
-	plugins: [
-		new webpack.ProvidePlugin({
-			$: 'jquery',
-			jQuery: 'jquery',
-			'window.jQuery': 'jquery',
-		}),
-		new VueLoaderPlugin(),
-		new MiniCssExtractPlugin({
-			filename: devMode ? 'styles/[name].css' : 'styles/[name].[contenthash].css',
-			chunkFilename: devMode ? '[id].css' : '[id].[contenthash].css',
-		}),
-		new CopyWebpackPlugin({
-			patterns: [
-				{
-					from: config.path.assets + '/images',
-					to: devMode ? 'images/[path][name][ext]' : 'images/[path][name].[contenthash][ext]',
-					globOptions: {
-						ignore: ['.gitkeep'],
-					},
-				},
-			],
-		}),
+    mode: env,
+    context: config.path.assets,
+    entry: config.entry,
+    devtool: CreateSourceMap ? 'inline-cheap-module-source-map' : false,
+    watch: watchMode,
+    output: {
+        filename: devMode ? 'scripts/[name].js' : 'scripts/[name].[contenthash].js',
+        chunkFilename: devMode ? 'scripts/[name].bundle.js' : 'scripts/[name].bundle.[contenthash].js',
+        path: config.path.dist,
+        publicPath: config.path.public,
+        pathinfo: false,
+    },
+    performance: { hints: false },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules[\/\\](?!(swiper|dom7|sal.js)[\/\\])/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true,
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '../',
+                        },
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                config: path.resolve(__dirname, 'postcss.config.js'),
+                            },
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.(ttf|eot|woff2?|png|jpe?g|gif|svg|ico)$/,
+                include: config.path.urlLoaderAssets,
+                loader: 'url-loader',
+                options: {
+                    limit: 4096,
+                    name: devMode ? '[path][name].[ext]' : '[path][name].[contenthash].[ext]',
+                },
+            },
+        ],
+    },
+    resolve: {
+        alias: {
+            acfBlocks: path.resolve(__dirname, config.path.theme + '/partials/blocks'),
+            twigBlocks: path.resolve(__dirname, config.path.theme + '/resources/views/blocks'),
+            parentThemeStyles: path.resolve(__dirname, config.path.parentThemeAssets + 'styles'),
+            parentThemeScripts: path.resolve(__dirname, config.path.parentThemeAssets + 'scripts'),
+            parentThemeFonts: path.resolve(__dirname, config.path.parentThemeAssets + 'fonts'),
+            parentThemeImages: path.resolve(__dirname, config.path.parentThemeAssets + 'images'),
+        },
+    },
+    externals: config.externals,
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+        }),
+        new VueLoaderPlugin(),
+        new MiniCssExtractPlugin({
+            filename: devMode ? 'styles/[name].css' : 'styles/[name].[contenthash].css',
+            chunkFilename: devMode ? '[id].css' : '[id].[contenthash].css',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: config.path.assets + '/images',
+                    to: devMode ? 'images/[path][name][ext]' : 'images/[path][name].[contenthash][ext]',
+                    globOptions: {
+                        ignore: ['.gitkeep'],
+                    },
+                },
+            ],
+        }),
 
-		new PalettePlugin({
-			output: 'palette.json',
-			blacklist: ['transparent', 'inherit'],
-			pretty: false,
-			sass: {
-				path: 'resources/assets/styles/1_common',
-				files: ['_variables.scss'],
-				variables: ['brand-colors'],
-			},
-		}),
+        new PalettePlugin({
+            output: 'palette.json',
+            blacklist: ['transparent', 'inherit'],
+            pretty: false,
+            sass: {
+                path: 'resources/assets/styles/1_common',
+                files: ['_variables.scss'],
+                variables: ['brand-colors'],
+            },
+        }),
 
-		new WebpackManifestPlugin({
-			publicPath: '',
-		}),
-	],
-	optimization: {
-		runtimeChunk: 'single',
-		splitChunks: {
-			cacheGroups: {
-				vendorBase: {
-					test: /_vendor.scss/,
-					name: "vendor-frontend",
-					chunks: 'all',
-					enforce: true,
-					automaticNameDelimiter: '-',
-					name: 'vendor',
-				},
-			},
-		},
-		minimizer: [
-			new TerserPlugin({
-				parallel: true,
-				terserOptions: {
-					output: {
-						comments: false,
-					},
-				},
-			}),
-		],
-	},
+        new WebpackManifestPlugin({
+            publicPath: '',
+        }),
+    ],
+    optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendorBase: {
+                    test: /_vendor.scss/,
+                    name: 'vendor-frontend',
+                    chunks: 'all',
+                    enforce: true,
+                    automaticNameDelimiter: '-',
+                    name: 'vendor',
+                },
+            },
+        },
+        minimizer: [
+            new TerserPlugin({
+                parallel: true,
+                terserOptions: {
+                    output: {
+                        comments: false,
+                    },
+                },
+            }),
+        ],
+    },
 };
 
 /**
@@ -177,7 +177,7 @@ const webpackConfig = {
  * @return {object}           updated webpackConfig configuration object.
  */
 if (watchMode) {
-	webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
+    webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
 }
 
 /**
@@ -187,38 +187,38 @@ if (watchMode) {
  * @return {object}           updated webpackConfig configuration object.
  */
 if (!devMode) {
-	webpackConfig.plugins.push(
-		new CleanWebpackPlugin(),
-		new ImageMinimizerPlugin({
-			minimizerOptions: {
-				// Lossless optimization with custom option
-				// Feel free to experiment with options for better result for you
-				plugins: [
-					['gifsicle', { interlaced: true }],
-					['jpegtran', { progressive: true }],
-					['optipng', { optimizationLevel: 5 }],
-					[
-						'svgo',
-						{
-							plugins: [
-								{
-									removeViewBox: false,
-								},
-							],
-						},
-					],
-				],
-			},
-		}),
-	);
+    webpackConfig.plugins.push(
+        new CleanWebpackPlugin(),
+        new ImageMinimizerPlugin({
+            minimizerOptions: {
+                // Lossless optimization with custom option
+                // Feel free to experiment with options for better result for you
+                plugins: [
+                    ['gifsicle', { interlaced: true }],
+                    ['jpegtran', { progressive: true }],
+                    ['optipng', { optimizationLevel: 5 }],
+                    [
+                        'svgo',
+                        {
+                            plugins: [
+                                {
+                                    removeViewBox: false,
+                                },
+                            ],
+                        },
+                    ],
+                ],
+            },
+        }),
+    );
 }
 
 if (!config.skipDependencyExtraction) {
-	webpackConfig.plugins.push(
-		new DependencyExtractionWebpackPlugin({
-			outputFormat: 'json',
-		}),
-	);
+    webpackConfig.plugins.push(
+        new DependencyExtractionWebpackPlugin({
+            outputFormat: 'json',
+        }),
+    );
 }
 
 module.exports = webpackConfig;
