554f353121c083bb3c37f67cce390cceb1c0883f
Erik van der Bas <erik@basedonline.nl>
2021-05-20 13:17:34 +0200

    configurable pallete path.

--- src/build/webpack.config.js
+++ src/build/webpack.config.js
@@ -25,155 +25,155 @@ const config = require('../config');
 const CreateSourceMap = devMode ? config.sourceMaps : false;
 
 const webpackConfig = {
-	mode: env,
-	context: config.path.assets,
-	entry: config.entry,
-	devtool: CreateSourceMap ? 'inline-cheap-module-source-map' : false,
-	watch: watchMode,
-	output: {
-		filename: devMode ? 'scripts/[name].js' : 'scripts/[name].[contenthash].js',
-		chunkFilename: devMode ? 'scripts/[name].bundle.js' : 'scripts/[name].bundle.[contenthash].js',
-		path: config.path.dist,
-		publicPath: config.path.public,
-		pathinfo: false,
-	},
-	performance: { hints: false },
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules[\/\\](?!(swiper|dom7|sal.js)[\/\\])/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						cacheDirectory: true,
-					},
-				},
-			},
-			{
-				test: /\.vue$/,
-				loader: 'vue-loader',
-			},
-			{
-				test: /\.scss$/,
-				exclude: /node_modules/,
-				use: [
-					{
-						loader: MiniCssExtractPlugin.loader,
-						options: {
-							publicPath: '../',
-						},
-					},
-					{
-						loader: 'css-loader',
-						options: {
-							sourceMap: CreateSourceMap,
-						},
-					},
-					{
-						loader: 'postcss-loader',
-						options: {
-							sourceMap: CreateSourceMap,
-							postcssOptions: {
-								config: path.resolve(__dirname, 'postcss.config.js'),
-							},
-						},
-					},
-					{
-						loader: 'sass-loader',
-						options: {
-							sourceMap: CreateSourceMap,
-						},
-					},
-				],
-			},
-			{
-				test: /\.(ttf|eot|woff2?|png|jpe?g|gif|svg|ico)$/,
-				include: config.path.urlLoaderAssets,
-				loader: 'url-loader',
-				options: {
-					limit: 4096,
-					name: devMode ? '[path][name].[ext]' : '[path][name].[contenthash].[ext]',
-				},
-			},
-		],
-	},
-	resolve: {
-		alias: {
-			acfBlocks: path.resolve(__dirname, config.path.theme + '/partials/blocks'),
-			twigBlocks: path.resolve(__dirname, config.path.theme + '/resources/views/blocks'),
-			parentThemeStyles: path.resolve(__dirname, config.path.parentThemeAssets + 'styles'),
-			parentThemeScripts: path.resolve(__dirname, config.path.parentThemeAssets + 'scripts'),
-			parentThemeFonts: path.resolve(__dirname, config.path.parentThemeAssets + 'fonts'),
-			parentThemeImages: path.resolve(__dirname, config.path.parentThemeAssets + 'images'),
-		},
-	},
-	externals: config.externals,
-	plugins: [
-		new webpack.ProvidePlugin({
-			$: 'jquery',
-			jQuery: 'jquery',
-			'window.jQuery': 'jquery',
-		}),
-		new VueLoaderPlugin(),
-		new MiniCssExtractPlugin({
-			filename: devMode ? 'styles/[name].css' : 'styles/[name].[contenthash].css',
-		}),
-		new CopyWebpackPlugin({
-			patterns: [
-				{
-					from: config.path.assets + '/images',
-					to: devMode ? 'images/[path][name].[ext]' : 'images/[path][name].[contenthash].[ext]',
-					globOptions: {
-						ignore: ['.gitkeep'],
-					},
-				},
-			],
-		}),
+    mode: env,
+    context: config.path.assets,
+    entry: config.entry,
+    devtool: CreateSourceMap ? 'inline-cheap-module-source-map' : false,
+    watch: watchMode,
+    output: {
+        filename: devMode ? 'scripts/[name].js' : 'scripts/[name].[contenthash].js',
+        chunkFilename: devMode ? 'scripts/[name].bundle.js' : 'scripts/[name].bundle.[contenthash].js',
+        path: config.path.dist,
+        publicPath: config.path.public,
+        pathinfo: false,
+    },
+    performance: { hints: false },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules[\/\\](?!(swiper|dom7|sal.js)[\/\\])/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true,
+                    },
+                },
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '../',
+                        },
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: CreateSourceMap,
+                        },
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: CreateSourceMap,
+                            postcssOptions: {
+                                config: path.resolve(__dirname, 'postcss.config.js'),
+                            },
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: CreateSourceMap,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(ttf|eot|woff2?|png|jpe?g|gif|svg|ico)$/,
+                include: config.path.urlLoaderAssets,
+                loader: 'url-loader',
+                options: {
+                    limit: 4096,
+                    name: devMode ? '[path][name].[ext]' : '[path][name].[contenthash].[ext]',
+                },
+            },
+        ],
+    },
+    resolve: {
+        alias: {
+            acfBlocks: path.resolve(__dirname, config.path.theme + '/partials/blocks'),
+            twigBlocks: path.resolve(__dirname, config.path.theme + '/resources/views/blocks'),
+            parentThemeStyles: path.resolve(__dirname, config.path.parentThemeAssets + 'styles'),
+            parentThemeScripts: path.resolve(__dirname, config.path.parentThemeAssets + 'scripts'),
+            parentThemeFonts: path.resolve(__dirname, config.path.parentThemeAssets + 'fonts'),
+            parentThemeImages: path.resolve(__dirname, config.path.parentThemeAssets + 'images'),
+        },
+    },
+    externals: config.externals,
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+        }),
+        new VueLoaderPlugin(),
+        new MiniCssExtractPlugin({
+            filename: devMode ? 'styles/[name].css' : 'styles/[name].[contenthash].css',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: config.path.assets + '/images',
+                    to: devMode ? 'images/[path][name].[ext]' : 'images/[path][name].[contenthash].[ext]',
+                    globOptions: {
+                        ignore: ['.gitkeep'],
+                    },
+                },
+            ],
+        }),
 
-		new PalettePlugin({
-			output: 'palette.json',
-			blacklist: ['transparent', 'inherit'],
-			pretty: false,
-			sass: {
-				path: 'resources/assets/styles/1_common',
-				files: ['_variables.scss'],
-				variables: ['brand-colors'],
-			},
-		}),
+        new PalettePlugin({
+            output: 'palette.json',
+            blacklist: ['transparent', 'inherit'],
+            pretty: false,
+            sass: {
+                path: config.path.scssSettingsFolder,
+                files: ['_variables.scss'],
+                variables: ['brand-colors'],
+            },
+        }),
 
-		new WebpackManifestPlugin({
-			publicPath: '',
-		}),
-	],
-	optimization: {
-		splitChunks: {
-			chunks: 'all',
-			automaticNameDelimiter: '-',
-			name: 'vendor',
-			cacheGroups: {
-				vendorBase: {
-					test: /_ie-legacy.scss/,
-					name: "ie-legacy",
-					chunks: 'all',
-					enforce: true,
-					reuseExistingChunk: false,
-				},
-			},
-		},
-		minimizer: [
-			new TerserPlugin({
-				cache: true,
-				parallel: true,
-				sourceMap: CreateSourceMap,
-				terserOptions: {
-					output: {
-						comments: false,
-					},
-				},
-			}),
-		],
-	},
+        new WebpackManifestPlugin({
+            publicPath: '',
+        }),
+    ],
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+            automaticNameDelimiter: '-',
+            name: 'vendor',
+            cacheGroups: {
+                vendorBase: {
+                    test: /_ie-legacy.scss/,
+                    name: 'ie-legacy',
+                    chunks: 'all',
+                    enforce: true,
+                    reuseExistingChunk: false,
+                },
+            },
+        },
+        minimizer: [
+            new TerserPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: CreateSourceMap,
+                terserOptions: {
+                    output: {
+                        comments: false,
+                    },
+                },
+            }),
+        ],
+    },
 };
 
 /**
@@ -186,7 +186,7 @@ const webpackConfig = {
  * @return {object}           updated webpackConfig configuration object.
  */
 if (watchMode) {
-	webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
+    webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
 }
 
 /**
@@ -196,37 +196,37 @@ if (watchMode) {
  * @return {object}           updated webpackConfig configuration object.
  */
 if (!devMode) {
-	webpackConfig.plugins.push(
-		new CleanWebpackPlugin(),
-		new ImageMinimizerPlugin({
-			minimizerOptions: {
-				// Lossless optimization with custom option
-				// Feel free to experement with options for better result for you
-				plugins: [
-					['gifsicle', { interlaced: true }],
-					['jpegtran', { progressive: true }],
-					['optipng', { optimizationLevel: 1 }],
-					[
-						'svgo',
-						{
-							plugins: [
-								{
-									removeViewBox: false,
-								},
-							],
-						},
-					],
-				],
-			},
-		}),
-	);
+    webpackConfig.plugins.push(
+        new CleanWebpackPlugin(),
+        new ImageMinimizerPlugin({
+            minimizerOptions: {
+                // Lossless optimization with custom option
+                // Feel free to experement with options for better result for you
+                plugins: [
+                    ['gifsicle', { interlaced: true }],
+                    ['jpegtran', { progressive: true }],
+                    ['optipng', { optimizationLevel: 1 }],
+                    [
+                        'svgo',
+                        {
+                            plugins: [
+                                {
+                                    removeViewBox: false,
+                                },
+                            ],
+                        },
+                    ],
+                ],
+            },
+        }),
+    );
 }
 if (!config.skipDependencyExtraction) {
-	webpackConfig.plugins.push(
-		new DependencyExtractionWebpackPlugin({
-			outputFormat: 'json',
-		}),
-	);
+    webpackConfig.plugins.push(
+        new DependencyExtractionWebpackPlugin({
+            outputFormat: 'json',
+        }),
+    );
 }
 
 module.exports = webpackConfig;
--- src/config.js
+++ src/config.js
@@ -17,28 +17,35 @@ var userConfigPath = '/assets/config.json';
  * If not we'll fall back to the default path themeDir/assets/config.json
  */
 if (process.env.npm_package_config_userConfig) {
-	userConfigPath = process.env.npm_package_config_userConfig;
+    userConfigPath = process.env.npm_package_config_userConfig;
 }
 
 try {
-	require.resolve((__dirname, rootPath) + userConfigPath);
+    require.resolve((__dirname, rootPath) + userConfigPath);
 } catch (e) {
-	console.log('\n❌ ', chalk.black.bgRed('Error locating "' + userConfigPath + '".'));
-	console.log('Did you create the config.json file in set location already?\n');
-	process.exit();
+    console.log('\n❌ ', chalk.black.bgRed('Error locating "' + userConfigPath + '".'));
+    console.log('Did you create the config.json file in set location already?\n');
+    process.exit();
 }
 userConfig = require(path.resolve(__dirname, rootPath) + userConfigPath);
+
+if (!userConfig['scssSettingsFolder']) {
+    console.log('\n❌ ', chalk.black.bgRed('Variable scssSettingsFolder not set in config.json'));
+    console.log('This is probably 01-settings/ or 1_common/ \n');
+    process.exit(1);
+}
+
 /**
  * Check if root to theme path is set.
  * Sets up proper publicPath and removes extra slashes from the url.
  */
 if (userConfig['rootToThemePath']) {
-	var publicPath = 'http://localhost:3000/' + userConfig['rootToThemePath'] + '/dist/';
-	var publicPath = publicPath.replace(/([^:])(\/\/+)/g, '$1/');
+    var publicPath = 'http://localhost:3000/' + userConfig['rootToThemePath'] + '/dist/';
+    publicPath = publicPath.replace(/([^:])(\/\/+)/g, '$1/');
 } else {
-	console.log('\n❌ ', chalk.black.bgRed('Variable rootToThemePath not set in config.json'));
-	console.log('This is probably /app/themes/YOURTHEMENAME/ or /wp-content/themes/YOURTHEMENAME/ \n');
-	process.exit(1);
+    console.log('\n❌ ', chalk.black.bgRed('Variable rootToThemePath not set in config.json'));
+    console.log('This is probably /app/themes/YOURTHEMENAME/ or /wp-content/themes/YOURTHEMENAME/ \n');
+    process.exit(1);
 }
 
 var externals = {};
@@ -48,29 +55,32 @@ var externals = {};
  * we overwrite the themePath var with this new path.
  */
 if (!userConfig['skipDependencyExtraction']) {
-	externals = { $: 'jquery', jQuery: 'jquery', 'window.jQuery': 'jquery' };
+    externals = { $: 'jquery', jQuery: 'jquery', 'window.jQuery': 'jquery' };
 }
+
+var assetsPath = userConfig['assetsPath'];
 
 /**
  * If the /assets/config.json file has a themePath option
  * we overwrite the themePath var with this new path.
  */
 if (userConfig['themePath']) {
-	themePath = userConfig['themePath'];
+    themePath = userConfig['themePath'];
 }
 
 var config = merge(
-	{
-		path: {
-			theme: path.join(rootPath, themePath), // from root folder path/to/theme
-			dist: path.join(rootPath, themePath + '/dist/'), // from root folder path/to/theme
-			assets: path.join(rootPath, userConfig['assetsPath']), // from folder containing the package.json to the theme folder.
-			urlLoaderAssets: [path.join(rootPath, userConfig['assetsPath'])], // create path for the url-loader. When we have a parent/child theme going we'll add the parent theme assets later on.
-			public: publicPath, // Used for webpack.output.publicpath - Had to be set this way to overcome middleware issues with dynamic path.
-		},
-		externals: externals,
-	},
-	userConfig,
+    {
+        path: {
+            theme: path.join(rootPath, themePath), // from root folder path/to/theme
+            scssSettingsFolder: [path.join(assetsPath, 'styles', userConfig['scssSettingsFolder'])],
+            dist: path.join(rootPath, themePath, 'dist'), // from root folder path/to/theme
+            assets: path.join(rootPath, assetsPath), // from folder containing the package.json to the theme folder.
+            urlLoaderAssets: [path.join(rootPath, assetsPath)], // create path for the url-loader. When we have a parent/child theme going we'll add the parent theme assets later on.
+            public: publicPath, // Used for webpack.output.publicpath - Had to be set this way to overcome middleware issues with dynamic path.
+        },
+        externals: externals,
+    },
+    userConfig,
 );
 
 /**
@@ -78,36 +88,36 @@ var config = merge(
  * setup we add extra paths to the config.path var.
  */
 if (userConfig['parentTheme']) {
-	config = merge(
-		{
-			path: {
-				parentTheme: path.join(rootPath, '../' + userConfig['parentTheme']),
-				parentThemeAssets: path.join(rootPath, '../' + userConfig['parentTheme'] + userConfig['assetsPath']),
-			},
-			skipDependencyExtraction: skipDependencyExtraction,
-		},
-		config,
-	);
-	/**
-	 * Add parent theme assets folder when we actually use a parent/child setup.
-	 */
-	config.path.urlLoaderAssets.push(config.path.parentThemeAssets);
+    config = merge(
+        {
+            path: {
+                parentTheme: path.join(rootPath, '../' + userConfig['parentTheme']),
+                parentThemeAssets: path.join(rootPath, '../' + userConfig['parentTheme'] + userConfig['assetsPath']),
+            },
+            skipDependencyExtraction: skipDependencyExtraction,
+        },
+        config,
+    );
+    /**
+     * Add parent theme assets folder when we actually use a parent/child setup.
+     */
+    config.path.urlLoaderAssets.push(config.path.parentThemeAssets);
 }
 
 /**
  * Pushes hot middleware client to our entry when in watchmode.
  */
 if (watchMode) {
-	config.entry.app.push('webpack-hot-middleware/client');
+    config.entry.app.push('webpack-hot-middleware/client');
 }
 
 /**
  * Pushes our entry file to the start of the entry array.
  */
 if (options.isTestRun) {
-	config.entry.app.unshift('../../src/helpers/publicpath-entry.js');
+    config.entry.app.unshift('../../src/helpers/publicpath-entry.js');
 } else {
-	config.entry.app.unshift('basebuilder-config/src/helpers/publicpath-entry.js');
+    config.entry.app.unshift('basebuilder-config/src/helpers/publicpath-entry.js');
 }
 
 module.exports = config;
