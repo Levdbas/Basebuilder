a9c4e2485e368e7284e59473d032395f2057753a
Erik van der Bas <erik@basedonline.nl>
2021-09-29 21:46:07 +0200

    font test

---
+++ tests/index.php
@@ -0,0 +1,26 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/functions.php';
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <link rel="stylesheet" href="<?php the_asset('app.css'); ?>">
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>HTML 5 Boilerplate</title>
+</head>
+
+<body>
+   <div class="icon-logo-linkedin"></div>
+
+
+   <script src="<?php the_asset('vendor.js'); ?>"></script>
+   <script src="<?php the_asset('app.js'); ?>"></script>
+</body>
+
+</html>
