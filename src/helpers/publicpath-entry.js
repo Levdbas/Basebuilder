1c11b22dc72066b4a67a6ce723f83b23f8faa3f6
Erik van der Bas <erik@basedonline.nl>
2024-03-18 10:57:34 +0100

    4.5.0

--- src/helpers/publicpath-entry.js
+++ src/helpers/publicpath-entry.js
@@ -1,6 +1,9 @@
 /**
  * Setting entrypath by checking the variable bp_site.
  */
-if (window.bp_site) {
+if (window.basebuilder) {
+	__webpack_public_path__ = window.basebuilder['dist'];
+}
+else if (window.bp_site) {
 	__webpack_public_path__ = window.bp_site['dist'];
 }
